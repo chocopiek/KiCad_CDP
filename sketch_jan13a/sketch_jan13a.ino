147a6cf8670206adb6545b7931e57840710fee98
Tran Anh Khoi <anhkhoi2006vtg@gmail.com>
2026-01-15 12:41:12 +0700

    testcommit

--- sketch_jan13a/sketch_jan13a.ino
+++ sketch_jan13a/sketch_jan13a.ino
@@ -6,23 +6,18 @@ const char* password = "7T03m=88";
 
 WebServer server(80);
 
-const int ledPin = 2;  // LED onboard ESP32
-
-void handleOn() {
-  Serial.println(">> Nhan lenh: ON");
-  digitalWrite(ledPin, HIGH);
-  server.send(200, "text/plain", "LED ON");
-}
-
-void handleOff() {
-  Serial.println(">> Nhan lenh: OFF");
-  digitalWrite(ledPin, LOW);
-  server.send(200, "text/plain", "LED OFF");
-}
+const int tempPin = 23;   // GPIO23 (ADC)
 
 void handleStatus() {
-  float temp = 27.5;   // ví dụ, sau này thay bằng cảm biến thật
-  String msg = "Temp=" + String(temp);
+  int adcValue = analogRead(tempPin);
+
+  // ESP32 ADC: 0–4095 tương ứng 0–3.3V
+  float voltage = adcValue * (3.3 / 4095.0);
+
+  // LM35: 10mV = 1°C
+  float temperature = voltage * 100.0;
+
+  String msg = "Temp=" + String(temperature, 1) + "C";
 
   Serial.println(">> Gui ve: " + msg);
   server.send(200, "text/plain", msg);
@@ -30,7 +25,9 @@ void handleStatus() {
 
 void setup() {
   Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
+
+  analogReadResolution(12); // 12-bit ADC
+  pinMode(tempPin, INPUT);
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -40,11 +37,9 @@ void setup() {
   Serial.print("ESP32 IP: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/on", handleOn);
-  server.on("/off", handleOff);
   server.on("/status", handleStatus);
-
   server.begin();
+
   Serial.println("HTTP server started");
 }
 
